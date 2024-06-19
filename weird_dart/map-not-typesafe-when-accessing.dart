d10b065c8bebcd9fb84b8ba93e56972f410a999b
tolotrasamuel <rsamuel15@alustudent.com>
2024-06-19 17:46:42 -0400

    Update map-not-typesafe-when-accessing.dart

--- weird_dart/map-not-typesafe-when-accessing.dart
+++ weird_dart/map-not-typesafe-when-accessing.dart
@@ -1,3 +1,5 @@
     final foo = <int, int>{};
     foo["123"] = "123"; // error when setting
     final d = foo["123"]; // no type error when getting
+
+/// btw dart equality is not type safe too . T == Object
